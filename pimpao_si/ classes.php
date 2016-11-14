e5679581f4357b1ec08a28e77762e8981b8b7ba6
Jaff Cunha <jaffgustavo@gmail.com>
2016-11-14 00:03:34 -0200

    Create classe.php

---
+++ pimpao_si/ classes.php
@@ -0,0 +1,34 @@
+<html>
+classes
+<?php
+    class Vendedora{
+        public $nome;
+        public $telefone;
+        public $email;
+    }
+    class Proprietaria{
+        public $nome;
+        public $telefone;
+        public $email;
+    }
+    class Produto{
+        public $nome;
+        public $codigo;
+        public $preco;
+    }
+    class Venda{
+        public $valor;
+        public $data;
+    }
+
+    // include("cliente-class.php");
+    //
+    // $Vendedora = new Vendedora();
+    // $Vendedora->nome = "Maria";
+    // $Vendedora->telefone = 11912344321;
+    // $Vendedora->email = "bla@gmail.com";
+    //
+    // echo "<br/>Nome da vendedora : ".$Vendedora->nome;
+    // echo "<br/>Email da vendedora : ".$Vendedora->email;
+?>
+</html>
