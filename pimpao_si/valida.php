514db980a121f3da99e20c2e51e51067502b9a48
paulodante <paulodante07@gmail.com>
2016-11-15 23:58:29 -0200

    Login e logout

---
+++ pimpao_si/valida.php
@@ -0,0 +1,36 @@
+<?php 
+ 
+ // Inclui o arquivo com o sistema de segurança 
+ require_once("seguranca.php"); 
+  
+   
+ 
+ // Verifica se um formulário foi enviado 
+ if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+   // Salva duas variáveis com o que foi digitado no formulário 
+   // Detalhe: faz uma verificação com isset() pra saber se o campo foi preenchido 
+   $login = (isset($_POST['login'])) ? $_POST['login'] : ''; 
+   $senha = (isset($_POST['senha'])) ? $_POST['senha'] : ''; 
+   $nome= (isset ($_POST['nome'])) ? $_POST['nome']:'';
+ 
+ 
+	 
+
+   // Utiliza uma função criada no seguranca.php pra validar os dados digitados 
+   if (validaUsuario($login, $senha) == true) { 
+	 // O usuário e a senha digitados foram validados, manda pra página interna 
+
+		header("Location: 100-Cadastro.php");
+		}
+   
+ }
+   else { 
+     // O usuário e/ou a senha são inválidos, manda de volta pro form de login 
+     // Para alterar o endereço da página de login, verifique o arquivo seguranca.php 
+     expulsaVisitante(); 
+   } 
+ 
+
+ 
+ 
+?>
