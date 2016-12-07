daade0d4295e8e75f6c71fc402662d65e7fb8c02
Jaff Cunha <jaffgustavo@gmail.com>
2016-12-07 00:23:35 -0200

    Remove ../ de caminhos em Login

--- pimpao_si/login.php
+++ pimpao_si/login.php
@@ -1,22 +1,22 @@
 <html>
 
 <!-- FAVICON RELATED -->
-<link rel="apple-touch-icon" sizes="57x57" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="apple-touch-icon" sizes="60x60" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="apple-touch-icon" sizes="72x72" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="apple-touch-icon" sizes="76x76" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="apple-touch-icon" sizes="114x114" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="apple-touch-icon" sizes="120x120" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="apple-touch-icon" sizes="144x144" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="apple-touch-icon" sizes="152x152" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="apple-touch-icon" sizes="180x180" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="icon" type="image/png" sizes="192x192"  href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="icon" type="image/png" sizes="32x32" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="icon" type="image/png" sizes="96x96" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="icon" type="image/png" sizes="16x16" href="../static/custom/favicon/ms-icon-144x144-pimpao.png">
-<link rel="manifest" href="../static/custom/favicon/manifest.json">
+<link rel="apple-touch-icon" sizes="57x57" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="apple-touch-icon" sizes="60x60" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="apple-touch-icon" sizes="72x72" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="apple-touch-icon" sizes="76x76" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="apple-touch-icon" sizes="114x114" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="apple-touch-icon" sizes="120x120" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="apple-touch-icon" sizes="144x144" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="apple-touch-icon" sizes="152x152" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="apple-touch-icon" sizes="180x180" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="icon" type="image/png" sizes="32x32" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="icon" type="image/png" sizes="96x96" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="icon" type="image/png" sizes="16x16" href="static/custom/favicon/ms-icon-144x144-pimpao.png">
+<link rel="manifest" href="static/custom/favicon/manifest.json">
 <meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="../static/custom/favicon/balaozinho.png">
+<meta name="msapplication-TileImage" content="static/custom/favicon/balaozinho.png">
 <meta name="theme-color" content="#ffffff">
 
 
@@ -33,19 +33,19 @@
 <meta content="" name="description" />
 <meta content="" name="author" />
 <!-- BEGIN PLUGIN CSS -->
-<link href="../static/demo/HTML/assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen"/>
-<link href="../static/demo/HTML/assets/plugins/bootstrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
-<link href="../static/demo/HTML/assets/plugins/bootstrapv3/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
-<link href="../static/demo/HTML/assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
-<link href="../static/demo/HTML/assets/plugins/animate.min.css" rel="stylesheet" type="text/css"/>
-<link href="../static/demo/HTML/assets/plugins/jquery-scrollbar/jquery.scrollbar.css" rel="stylesheet" type="text/css"/>
+<link href="static/demo/HTML/assets/plugins/pace/pace-theme-flash.css" rel="stylesheet" type="text/css" media="screen"/>
+<link href="static/demo/HTML/assets/plugins/bootstrapv3/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+<link href="static/demo/HTML/assets/plugins/bootstrapv3/css/bootstrap-theme.min.css" rel="stylesheet" type="text/css"/>
+<link href="static/demo/HTML/assets/plugins/font-awesome/css/font-awesome.css" rel="stylesheet" type="text/css"/>
+<link href="static/demo/HTML/assets/plugins/animate.min.css" rel="stylesheet" type="text/css"/>
+<link href="static/demo/HTML/assets/plugins/jquery-scrollbar/jquery.scrollbar.css" rel="stylesheet" type="text/css"/>
 <!-- END PLUGIN CSS -->
 <!-- BEGIN CORE CSS FRAMEWORK -->
-<link href="../static/demo/HTML/webarch/css/webarch.css" rel="stylesheet" type="text/css"/>
+<link href="static/demo/HTML/webarch/css/webarch.css" rel="stylesheet" type="text/css"/>
 <!-- END CORE CSS FRAMEWORK -->
 
 <!-- Begin notifications -->
-<link rel="stylesheet" type="text/css" href="../static/demo/HTML/assets/plugins/bootstrap-toastr/toastr.min.css"/>
+<link rel="stylesheet" type="text/css" href="static/demo/HTML/assets/plugins/bootstrap-toastr/toastr.min.css"/>
 <!-- End notifications -->
 
 </head>
@@ -54,7 +54,7 @@
 <!-- BEGIN BODY -->
 <!--<body style="background-color: #339933">-->
 <!-- PARA COLOCAR IMAGEM NO FUNDO -->
-<body class="error-body no-top lazy"  data-original="../static/custom/background3.png"  style="background-image: url('../static/custom/favicon/balaozinho.png')">
+<body class="error-body no-top lazy"  data-original="static/custom/background3.png"  style="background-image: url('static/custom/favicon/balaozinho.png')">
 
 <div class="container">
   <div class="row login-container animated fadeInUp">
@@ -96,20 +96,20 @@
 </div>
 <!-- END CONTAINER -->
 
-<script src="../static/demo/HTML/assets/plugins/pace/pace.min.js" type="text/javascript"></script>
+<script src="static/demo/HTML/assets/plugins/pace/pace.min.js" type="text/javascript"></script>
 <!-- BEGIN JS DEPENDECENCIES-->
-<script src="../static/demo/HTML/assets/plugins/jquery/jquery-1.11.3.min.js" type="text/javascript"></script>
-<script src="../static/demo/HTML/assets/plugins/bootstrapv3/js/bootstrap.min.js" type="text/javascript"></script>
-<script src="../static/demo/HTML/assets/plugins/jquery-block-ui/jqueryblockui.min.js" type="text/javascript"></script>
-<script src="../static/demo/HTML/assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
-<script src="../static/demo/HTML/assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js" type="text/javascript"></script>
-<script src="../static/demo/HTML/assets/plugins/jquery-numberAnimate/jquery.animateNumbers.js" type="text/javascript"></script>
-<script src="../static/demo/HTML/assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
-<script src="../static/demo/HTML/assets/plugins/bootstrap-select2/select2.min.js" type="text/javascript"></script>
+<script src="static/demo/HTML/assets/plugins/jquery/jquery-1.11.3.min.js" type="text/javascript"></script>
+<script src="static/demo/HTML/assets/plugins/bootstrapv3/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="static/demo/HTML/assets/plugins/jquery-block-ui/jqueryblockui.min.js" type="text/javascript"></script>
+<script src="static/demo/HTML/assets/plugins/jquery-unveil/jquery.unveil.min.js" type="text/javascript"></script>
+<script src="static/demo/HTML/assets/plugins/jquery-scrollbar/jquery.scrollbar.min.js" type="text/javascript"></script>
+<script src="static/demo/HTML/assets/plugins/jquery-numberAnimate/jquery.animateNumbers.js" type="text/javascript"></script>
+<script src="static/demo/HTML/assets/plugins/jquery-validation/js/jquery.validate.min.js" type="text/javascript"></script>
+<script src="static/demo/HTML/assets/plugins/bootstrap-select2/select2.min.js" type="text/javascript"></script>
 <!-- END CORE JS DEPENDECENCIES-->
 <!-- BEGIN CORE TEMPLATE JS -->
-<script src="../static/demo/HTML/webarch/js/webarch.js" type="text/javascript"></script>
-<script src="../static/demo/HTML/assets/js/chat.js" type="text/javascript"></script>
+<script src="static/demo/HTML/webarch/js/webarch.js" type="text/javascript"></script>
+<script src="static/demo/HTML/assets/js/chat.js" type="text/javascript"></script>
 <!-- END CORE TEMPLATE JS -->
 
 </body>
